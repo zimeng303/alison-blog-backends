2698b073c070becb429f6b3d1c46ab8486b203dd
zimeng303 <li_shengnan0303@163.com>
2021-02-04 13:13:22 +0800

    数据库配置

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ connections: {
       client: 'mysql',
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'alison-blog'),
+      database: env('DATABASE_NAME', 'alisonblog'),
       username: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', '12345678'),
     },
